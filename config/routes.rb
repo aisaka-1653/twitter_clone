d8bee7c2420747016b819985e903d0b8a1a13e56
aisaka-1653 <reobata1127@gmail.com>
2024-04-13 18:24:19 +0900

    homesコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@
 
 Rails.application.routes.draw do
   devise_for :users
+  root 'homes#index'
+  resources :homes, only: %i[index]
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
