cf6b8ea265cb0b732095b83956d611bc40b8ab2a
aisaka-1653 <reobata1127@gmail.com>
2024-04-16 23:02:14 +0900

    メール送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#index'
   resources :homes, only: %i[index]
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
