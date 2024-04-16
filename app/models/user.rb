e1f42f46256067b0237bf0f88a9499b5e9090913
aisaka-1653 <reobata1127@gmail.com>
2024-04-16 23:16:45 +0900

    devise moduleを全て有効にする

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, :confirmable, :trackable,
+         :timeoutable, :lockable
 
   validates :email, :display_name, :username, :date_of_birth, :mobile_number, presence: true
 end
