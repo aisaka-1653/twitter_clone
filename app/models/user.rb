41f9c5479e9ea422a1bcb44136c4d58ccf1f3128
aisaka-1653 <reobata1127@gmail.com>
2024-04-20 13:31:50 +0900

    OAuthの設定を追加し認証ページへのリンクを有効化

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, :confirmable, :trackable,
-         :timeoutable, :lockable
+         :confirmable, :trackable, :timeoutable,
+         :lockable, :omniauthable, omniauth_providers: %i[github]
 
   validates :email, :display_name, :username, :date_of_birth, :mobile_number, presence: true
 end
