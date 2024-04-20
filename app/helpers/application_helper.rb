48f3a31319a9b47f48a4c5c53043b3bf084d719f
aisaka-1653 <reobata1127@gmail.com>
2024-04-20 12:03:32 +0900

    脆弱性の観点からhtml_safeメソッドの削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,4 @@ module ApplicationHelper
   def hidden_field_tag(_name, _value = nil, _options = {})
     raise 'Happiness chainではhidden_field_tagの使用を禁止しています'
   end
-
-  def render_flash_messages(flash)
-    flash.map do |type, message|
-      content_tag(:p, message, class: "alert alert-#{type == 'notice' ? 'success' : 'danger'} text-center")
-    end.join.html_safe
-  end
 end
