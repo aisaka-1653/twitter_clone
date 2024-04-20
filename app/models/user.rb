7b2cd472b69f6f3f6222c0fe0b32cc657797f5e8
aisaka-1653 <reobata1127@gmail.com>
2024-04-20 21:06:04 +0900

    OmniAuth Callbackを使用してGitHubログインを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,17 @@ class User < ApplicationRecord
 
   validates :email, :display_name, :username, :date_of_birth, :mobile_number, presence: true
   validates :uid, uniqueness: { scope: :provider }
+
+  def self.from_omniauth(auth)
+    find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.display_name = auth.info.name
+      # user.image = auth.info.image
+      user.username = auth.info.nickname
+      # メアドによるアカウント登録では入力必須のためダミーデータを入れる
+      user.date_of_birth = '1900-01-01'
+      user.mobile_number = '00011112222'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'homes#index'
   resources :homes, only: %i[index]
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
