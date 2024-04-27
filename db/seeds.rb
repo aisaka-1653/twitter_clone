a4b399b7aed3b4ae3a728bdb3e3eb3f1685c5e02
aisaka-1653 <reobata1127@gmail.com>
2024-04-27 13:58:28 +0900

    seedを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,24 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+ApplicationRecord.transaction do
+  (1..5).each do |n| 
+    user = User.create!(
+      email: "user#{n}@example.com",
+      password: "123456",
+      password_confirmation: "123456",
+      display_name: "display_name#{n}",
+      username: "username#{n}",
+      date_of_birth: "1997-11-27",
+      mobile_number: "00011112222",
+    )
+    user.tweets.create!(
+      content: "Twitterを始めました｡#{user.display_name}です｡"
+    )
+  end
+end
+
+User.all.each do |user|
+  next_user = User.find_by(id: user.id + 1)
+  next_user = User.first if next_user.nil?
+  Follow.create!(follower: user, followee: next_user)
+end
