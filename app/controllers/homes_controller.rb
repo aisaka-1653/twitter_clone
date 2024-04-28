2dfe9766ec147d48dd955b1abcdd7d0d6270e19b
aisaka-1653 <reobata1127@gmail.com>
2024-04-28 14:40:46 +0900

    タイムラインの実装

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class HomesController < ApplicationController
-  def index; end
+  def index
+    @all_tweets = Tweet.order(created_at: :desc)
+    @following_tweets = Tweet.where(user_id: current_user.following_user_ids).order(created_at: :desc)
+  end
 end
