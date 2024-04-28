85dced236c2fbb7f4d2f86d6880b55efa5bdd33e
aisaka-1653 <reobata1127@gmail.com>
2024-04-28 17:52:51 +0900

    paginationの実装

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,7 +2,7 @@
 
 class HomesController < ApplicationController
   def index
-    @all_tweets = Tweet.order(created_at: :desc)
-    @following_tweets = Tweet.where(user_id: current_user.following_user_ids).order(created_at: :desc)
+    @all_tweets = Tweet.order(created_at: :desc).page(params[:all_page])
+    @following_tweets = Tweet.where(user_id: current_user.following_user_ids).order(created_at: :desc).page(params[:following_page])
   end
 end
