60f22f8cad5f49b6642cb28fcf675a4a0f40eecb
aisaka-1653 <reobata1127@gmail.com>
2024-04-30 17:31:09 +0900

    プロフィール画面用のseedを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 
 ApplicationRecord.transaction do
   (1..5).each do |n|
-    user = User.create!(
+    user = User.new(
       email: "user#{n}@example.com",
       password: '123456',
       password_confirmation: '123456',
@@ -11,16 +11,30 @@ ApplicationRecord.transaction do
       date_of_birth: '1997-11-27',
       mobile_number: '00011112222'
     )
+    user.skip_confirmation!
+    user.save!
     (1..10).each do |i|
       user.tweets.create!(
-        content: "Twitterを始めました｡#{user.display_name}です｡#{i}回目のツイートです｡"
+        content: "#{user.display_name}です｡#{i}回目のツイートです｡"
       )
     end
   end
 end
 
-User.all.each do |user|
-  next_user = User.find_by(id: user.id + 1)
-  next_user = User.first if next_user.nil?
-  Follow.create!(follower: user, followee: next_user)
+ApplicationRecord.transaction do
+  User.find_each do |user|
+    User.where.not(id: user.id).find_each do |other_user|
+      Follow.create!(follower: user, followee: other_user)
+      other_user.tweets.limit(5).find_each do |tweet|
+        user.likes.create!(tweet: tweet)
+        user.retweets.create!(tweet: tweet)
+        user.bookmarks.create!(tweet: tweet)
+        user.comments.create!(
+          tweet: tweet,
+          content: "#{other_user.display_name}さん!はじめまして!",
+          username: other_user.username
+        )
+      end
+    end
+  end
 end
