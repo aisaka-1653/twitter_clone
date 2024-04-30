21995b465b291059b83305a2fb90f4306d390736
aisaka-1653 <reobata1127@gmail.com>
2024-04-30 23:32:26 +0900

    削除ツイートはメンションを非表示にする

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
   def current_user?(user)
     current_user == user
   end
+
+  def username_present?(comment)
+    comment.respond_to?(:username) && comment.tweet_id.present?
+  end
 end
