bd9c3b29176f4fca0d53c1dfe1a537d8d564e9f2
aisaka-1653 <reobata1127@gmail.com>
2024-05-01 11:58:59 +0900

    エンゲージメントでアイコンの色を変更

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,16 @@ module UsersHelper
   def username_present?(comment)
     comment.respond_to?(:username) && comment.tweet_id.present?
   end
+
+  def liked_by_current_user?(tweet)
+    tweet.respond_to?(:likes) && tweet.likes.exists?(user_id: current_user.id)
+  end
+
+  def retweeted_by_current_user?(tweet)
+    tweet.respond_to?(:retweets) && tweet.retweets.exists?(user_id: current_user.id)
+  end
+
+  def bookmarked_by_current_user?(tweet)
+    tweet.respond_to?(:bookmarks) && tweet.bookmarks.exists?(user_id: current_user.id)
+  end
 end
