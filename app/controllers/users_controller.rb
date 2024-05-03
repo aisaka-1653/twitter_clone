2485f7fec6f487161d638f73ff55243445507283
aisaka-1653 <reobata1127@gmail.com>
2024-05-03 17:49:55 +0900

    プロフィール更新後にflashメッセージを表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to user_url(@user)
+      redirect_to user_url(@user), notice: 'プロフィールを更新しました'
     else
       render :edit, status: :unprocessable_entity
     end
