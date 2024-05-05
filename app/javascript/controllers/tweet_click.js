dfe698ec75765c918aa903dc0a7bc124f7ff8af3
aisaka-1653 <reobata1127@gmail.com>
2024-05-05 21:16:25 +0900

    ツイート詳細画面でツイートのクリックイベントを無効化

--- app/javascript/controllers/tweet_click.js
+++ app/javascript/controllers/tweet_click.js
@@ -3,16 +3,18 @@
   'use strict'
 
   window.addEventListener('turbo:load', () => {
-    document.querySelectorAll('.tweet').forEach(tweet => {
-      tweet.addEventListener('click', function(event) {
-        const ignoredClasses = ['.user-link', '.comment-link', '.rt-link', '.like-link', '.bookmark-link'];
-        const isIgnoredClick = ignoredClasses.some(className => event.target.closest(className));
+    if (!location.pathname.includes('/tweets/')) {
+      document.querySelectorAll('.tweet').forEach(tweet => {
+        tweet.addEventListener('click', function(event) {
+          const ignoredClasses = ['.user-link', '.comment-link', '.rt-link', '.like-link', '.bookmark-link'];
+          const isIgnoredClick = ignoredClasses.some(className => event.target.closest(className));
 
-        if (!isIgnoredClick) {
-          const tweetId = this.getAttribute('data-tweet-id');
-          window.location.href = `/tweets/${tweetId}`;
-        }
+          if (!isIgnoredClick) {
+            const tweetId = this.getAttribute('data-tweet-id');
+            window.location.href = `/tweets/${tweetId}`;
+          }
+        });
       });
-    });
+    }
   });
 })()
