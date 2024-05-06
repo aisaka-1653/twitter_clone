1cce2282603435efe1a334b9eeac03725691d15c
aisaka-1653 <reobata1127@gmail.com>
2024-05-06 18:17:35 +0900

    いいね総数を表示

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -4,4 +4,11 @@ module TweetsHelper
   def image_attached?(tweet)
     tweet.image.attached?
   end
+
+  def likes_count(tweet)
+    count = tweet.likes.count
+    if count > 0
+      content_tag(:span, count, class: 'text-secondary small')
+    end
+  end
 end
