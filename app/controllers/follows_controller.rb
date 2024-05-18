69d5a63e280d80a40e79cda73ad712a9b5e56c5d
aisaka-1653 <reobata1127@gmail.com>
2024-05-18 19:16:27 +0900

    [FollowsController]自身をフォローできないようにbefore_actionを追加

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,11 +1,25 @@
 class FollowsController < ApplicationController
+  before_action :set_user, :ensure_not_self!
+
   def create
-    current_user.followers.create(followee_id: params[:user_id])
+    current_user.followers.create(followee_id: @user.id)
     redirect_to request.referer
   end
 
   def destroy
-    current_user.followers.find_by(followee_id: params[:user_id]).destroy!
+    current_user.followers.find_by(followee_id: @user.id).destroy!
     redirect_to request.referer
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def ensure_not_self!
+    return unless current_user?(@user)
+
+    redirect_to request.referer, alert: '自身はフォローできません'
   end
 end
