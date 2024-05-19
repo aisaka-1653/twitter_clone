3020f463c32ac91256f771ba667a48b100f392a5
aisaka-1653 <reobata1127@gmail.com>
2024-05-19 12:03:55 +0900

    rubocop/slim-lintを適用

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class BookmarksController < InteractionsController
   def index
     @tweets = Tweet.preload_user_and_avatar(current_user.bookmarked_tweets).page(params[:page])
