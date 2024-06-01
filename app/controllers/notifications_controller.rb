56b80d92f2d2e64a0505a478b0a6b7829da06059
aisaka-1653 <reobata1127@gmail.com>
2024-06-01 14:57:17 +0900

    notifications_controllerのロジックを実装

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,13 @@
 class NotificationsController < ApplicationController
+  after_action :update_unread_notifications_to_read
+
   def index
+    @notifications = current_user.received_notifications.includes(sender: { avatar_attachment: :blob }, notifiable: [:tweet, :follower]).order(created_at: :desc).page(params[:page])
+  end
+  
+  private
+
+  def update_unread_notifications_to_read
+    @notifications.unread.update_all(checked: true)
   end
 end
