2b6207efc0df6611c617d349216fa86d4b76c923
aisaka-1653 <reobata1127@gmail.com>
2024-06-29 20:31:05 +0900

    [Controller]ユーザーログインテストのリファクタリング

--- spec/requests/users/sessions_spec.rb
+++ spec/requests/users/sessions_spec.rb
@@ -1,27 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Users::Sessions", type: :request do
-  let(:user) { create(:user) }
-
   describe "GET /users/sign_in" do
-    subject(:login_page_request) { get new_user_session_path }
-
     context 'ゲストユーザの場合' do
-      before { login_page_request }
-
       it 'ログイン画面を表示すること' do
+        get new_user_session_path
         expect(response).to have_http_status :ok
         expect(response.body).to include('ログインを記憶する')
       end
     end
 
     context 'ログインユーザの場合' do
-      before do
-        sign_in user
-        login_page_request
-      end
+      let(:user) { create(:user) }
+
+      before { sign_in user }
 
       it 'ホーム画面にリダイレクトすること' do
+        get new_user_session_path
         expect(response).to have_http_status :found
         expect(response).to redirect_to root_path
         expect(flash[:alert]).to eq 'すでにログインしています。'
@@ -30,13 +25,13 @@ RSpec.describe "Users::Sessions", type: :request do
   end
 
   describe "POST /users/sign_in" do
-    subject(:login_request) { post user_session_path, params: params }
+    let(:user) { create(:user) }
 
     context '有効なパラメータの場合' do
       let(:params) { { user: { email: user.email, password: user.password } } }
 
       it 'ログイン後､ホーム画面にリダイレクトすること' do
-        login_request
+        post user_session_path, params: params
         expect(response).to have_http_status :see_other
         expect(response).to redirect_to root_path
         expect(flash[:notice]).to eq 'ログインしました。'
@@ -46,10 +41,10 @@ RSpec.describe "Users::Sessions", type: :request do
     context '無効なパラメータの場合' do
       let(:params) { { user: { email: user.email, password: 'wrong_password' } } }
 
-      it 'ログイン画面をレンダリングすること' do
-        login_request
+      it 'ログインに失敗しflashメッセージを表示すること' do
+        post user_session_path, params: params
         expect(response).to have_http_status :unprocessable_entity
-        expect(response.body).to include('ログインを記憶する')
+        expect(flash[:alert]).to eq 'Eメールまたはパスワードが違います。'
       end
     end
   end
