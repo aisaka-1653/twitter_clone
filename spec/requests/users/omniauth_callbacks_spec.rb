b7fd5a3fbbb2dd6d90246c18a009c71fd8046a18
aisaka-1653 <reobata1127@gmail.com>
2024-06-29 20:53:53 +0900

    [Controller]GitHubログインテストのリファクタリング

--- spec/requests/users/omniauth_callbacks_spec.rb
+++ spec/requests/users/omniauth_callbacks_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Users::OmniauthCallbacks", type: :request do
-  describe "GET /users/auth/github/callback" do
-    subject(:github_callback_request) { get user_github_omniauth_callback_path }
+  describe "POST /users/auth/github/callback" do
 
     shared_examples 'successful authentication' do
       it 'ホーム画面にリダイレクトしてメッセージを表示すること' do
-        github_callback_request
+        post user_github_omniauth_callback_path
         expect(response).to have_http_status :found
         expect(response).to redirect_to root_path
         expect(flash[:notice]).to eq 'github アカウントによる認証に成功しました。'
@@ -14,25 +13,31 @@ RSpec.describe "Users::OmniauthCallbacks", type: :request do
     end
 
     context '認証に成功した場合' do
-      before do
+      before {
         stub_request(:get, "http://example.com/mock_user_avatar.png")
           .to_return(status: 200, body: File.new("spec/fixtures/mock_avatar.png"), headers: {'Content-Type' => 'image/png'})
         mock_github_auth
-      end
+      }
 
       context '初回ログインの場合' do
         it 'ユーザを作成してログインすること' do
-          expect { github_callback_request }.to change(User, :count).by(1)
+          expect {
+            post user_github_omniauth_callback_path
+          }.to change(User, :count).by(1)
         end
 
         it_behaves_like 'successful authentication'
       end
 
       context '2回目以降のログインの場合' do
-        before { github_callback_request }
+        before {
+          post user_github_omniauth_callback_path
+        }
 
         it 'ユーザを作成せずログインすること' do
-          expect { github_callback_request }.not_to change(User, :count)
+          expect {
+            post user_github_omniauth_callback_path
+          }.to_not change(User, :count)
         end
 
         it_behaves_like 'successful authentication'
@@ -42,9 +47,11 @@ RSpec.describe "Users::OmniauthCallbacks", type: :request do
     context '認証に失敗した場合' do
       before { mock_github_auth_failure }
 
-      it 'ログイン画面にリダイレクトすること' do
-        github_callback_request
+      it 'ログインに失敗しflashメッセージを表示すること' do
+        post user_github_omniauth_callback_path
         expect(response).to redirect_to root_path
+        follow_redirect!
+        expect(flash[:alert]).to eq 'ログインもしくはアカウント登録してください。'
       end
     end
   end
