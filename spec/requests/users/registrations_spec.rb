e2bf1e7228c9e1a4062aeb77a8429f1658ec5262
aisaka-1653 <reobata1127@gmail.com>
2024-06-29 20:04:51 +0900

    [Controller]ユーザー登録テストのリファクタリング

--- spec/requests/users/registrations_spec.rb
+++ spec/requests/users/registrations_spec.rb
@@ -1,27 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Users::Registrations", type: :request do
-  let(:user) { create(:user) }
-
   describe "GET /users/sign_up" do
-    subject(:sign_up_page_request) { get new_user_registration_path }
-
     context 'ゲストユーザの場合' do
-      before { sign_up_page_request }
-
       it '新規登録画面を表示すること' do
+        get new_user_registration_path
         expect(response).to have_http_status :ok
         expect(response.body).to include('電話番号')
       end
     end
 
     context 'ログインユーザの場合' do
-      before do
-        sign_in user
-        sign_up_page_request
-      end
+      let(:user) { create(:user) }
+
+      before { sign_in user }
 
       it 'ホーム画面にリダイレクトすること' do
+        get new_user_registration_path
         expect(response).to have_http_status :found
         expect(response).to redirect_to root_path
         expect(flash[:alert]).to eq "すでにログインしています。"
@@ -30,14 +25,14 @@ RSpec.describe "Users::Registrations", type: :request do
   end
 
   describe "POST /users/sign_up" do
-    subject(:sign_up_request) { post user_registration_path, params: params }
-
     context '有効なパラメータの場合' do
       let(:params) { { user: attributes_for(:user) } }
 
       it 'ユーザを作成し､認証メールを送信すること' do
-        expect { perform_enqueued_jobs { sign_up_request } }
-          .to change(User, :count).by(1)
+        expect { perform_enqueued_jobs {
+          post user_registration_path, params: params
+          }
+        } .to change(User, :count).by(1)
           .and change { ActionMailer::Base.deliveries.count }.by(1)
 
         mail = ActionMailer::Base.deliveries.last
@@ -47,12 +42,15 @@ RSpec.describe "Users::Registrations", type: :request do
     end
 
     context '無効なパラメータの場合' do
-      let(:params) { { user: attributes_for(:user, :invalid) } }
+      let(:params) { { user: attributes_for(:user, email: nil) } }
 
-      it 'ユーザを作成せず､新規登録画面をレンダリングすること' do
-        expect { sign_up_request }.to_not change(User, :count)
+      it 'ユーザを作成せず､バリデーションメッセージを表示すること' do
+        expect {
+          post user_registration_path, params: params
+        }.to_not change(User, :count)
+
         expect(response).to have_http_status :unprocessable_entity
-        expect(response.body).to include('電話番号')
+        expect(response.body).to include('Eメールを入力してください')
       end
     end
   end
